181c54d74d9cb3966f9490012ec8cf1009fc2d15
maazshaikh-tsl <maaz@telsourcelabs.com>
2025-03-03 15:36:10 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-set -e  # Exit on any error
 
-# Update system packages
-sudo dnf update -y
-
-# Install Apache (httpd)
-sudo dnf install -y httpd
-
-# Enable and start Apache service
-sudo systemctl enable --now httpd
-
-# Ensure Apache runs on startup
-sudo systemctl restart httpd
-
-echo "Dependencies installed successfully!"
